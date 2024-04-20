a02498ccf9c0a864dadbfe070d1ecb9396fb7325
swatisingh2002 <110125628+swatisingh2002@users.noreply.github.com>
2024-04-20 12:25:09 +0530

    update

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
-
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from flask import Flask, request, render_template, jsonify
+# Alternatively can use Django, FastAPI, or anything similar
+from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
-
 app = application
 
-## Route for the home page
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+@app.route('/predict', methods = ['POST', "GET"])
 
-@app.route("/")
-def index():
-    return render_template('index.html') 
-
-@app.route('/predictdata', methods = {'GET','POST'})
-def predict_datapoint():
-    if request.method =='GET':
-        return render_template('home.html')
-    else:
+def predict_datapoint(): 
+    if request.method == "GET": 
+        return render_template("form.html")
+    else: 
         data=CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
@@ -30,16 +24,15 @@ def predict_datapoint():
             writing_score=float(request.form.get('reading_score'))
 
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
+    new_data = data.get_data_as_dataframe()
+    predict_pipeline = PredictPipeline()
+    pred = predict_pipeline.predict(new_data)
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
+    results = round(pred[0],2)
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", debug = True)        
+    return render_template("results.html", final_result = results)
+
+if __name__ == "__main__": 
+    app.run(host = "0.0.0.0", debug= True)
+
+#http://127.0.0.1:5000/ in browser
