041925435021f23238ea7dd39c9654d5de088f5f
swatisingh2002 <110125628+swatisingh2002@users.noreply.github.com>
2024-04-20 11:27:41 +0530

    updates

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app = application
 
 ## Route for the home page
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata', methods = ['GET','POST'])
+@app.route('/predictdata', methods = {'GET','POST'})
 def predict_datapoint():
     if request.method =='GET':
         return render_template('home.html')
@@ -24,7 +24,7 @@ def predict_datapoint():
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
+            
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
